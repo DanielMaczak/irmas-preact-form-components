fb9c43bab501c8d2f7bb3777cafab755f4648914
DanielMaczak <daniel.maczak@proton.me>
2024-03-28 15:13:27 +0100

    feat(components): implement autocapitalize for text-input component
    apply autocapitalize via code to replace native HTML feature that works only as autocorrect on mobile

--- src/app.tsx
+++ src/app.tsx
@@ -7,9 +7,9 @@ import { ForwardedRef } from 'preact/compat';
 
 //  DEVELOPMENT OF GENERIC COMPONENTS
 // import { TextInput } from './components-generic/components/text-input.component';
-// import { DateInput } from './components-generic/components/date-input.component';
-import { NumInput } from './components-generic/components/num-input.component';
-import { ReplaceInput } from './components-generic/components/replace-input.component';
+import { DateInput } from './components-generic/components/date-input.component';
+// import { NumInput } from './components-generic/components/num-input.component';
+// import { ReplaceInput } from './components-generic/components/replace-input.component';
 // import { Ref } from 'preact';
 
 //  DEVELOPMENT OF GENERIC COMPONENTS
@@ -24,67 +24,68 @@ import { ReplaceInput } from './components-generic/components/replace-input.comp
 //     multiline={true}
 //   />
 // );
-// const DateInputSample = () => (
-//   <DateInput
-//     value={Date.now()}
-//     setValue={() => {}}
-//     className="date-class"
-//     label="date-label:"
-//     // enabled={false}
-//   />
-// );
-const NumInputSample = () => {
-  const [value, setValue] = useState(1234);
-  return (
-    <NumInput
-      value={value}
-      setValue={setValue}
-      className="num-class"
-      label="num-label:"
-      max={100}
-      invalidClassName="invalid-value"
-    />
-  );
-};
+const DateInputSample = () => (
+  <DateInput
+    value={Date.now()}
+    setValue={() => {}}
+    className="date-class"
+    label="date-label:"
+    // enabled={false}
+  />
+);
+// const NumInputSample = () => {
+//   const [value, setValue] = useState(1234);
+//   return (
+//     <NumInput
+//       value={value}
+//       setValue={setValue}
+//       className="num-class"
+//       label="num-label:"
+//       max={100}
+//       invalidClassName="invalid-value"
+//     />
+//   );
+// };
 
 const ReplaceTextInputSample = () => {
-  const [value, setValue] = useState(1234);
-  return (
-    // <ReplaceInput
-    // component={(ref: Ref<HTMLElement>) => (
-    //   <img
-    //     ref={ref as Ref<HTMLImageElement>}
-    //     src="https://cdn-icons-png.flaticon.com/512/347/347052.png"
-    //   />
-    // )}
-    // >
-    //   {(childRef: ForwardedRef<HTMLElement>) => (
-    //     <TextInput
-    //       value={value}
-    //       setValue={setValue}
-    //       className="text-class"
-    //       label="text-label:"
-    //       placeholder="text-placeholder"
-    //       autocapitalize="words"
-    //       multiline={true}
-    //       ref={childRef}
-    //     />
-    //   )}
-    // </ReplaceInput>
-    <ReplaceInput>
-      {(childRef: ForwardedRef<HTMLElement>) => (
-        <NumInput
-          value={value}
-          setValue={setValue}
-          className="num-class"
-          label="num-label:"
-          max={100}
-          invalidClassName="invalid-value"
-          ref={childRef}
-        />
-      )}
-    </ReplaceInput>
-  );
+  const [value, setValue] = useState('1234');
+  // return (
+  // <ReplaceInput
+  // component={(ref: Ref<HTMLElement>) => (
+  //   <img
+  //     ref={ref as Ref<HTMLImageElement>}
+  //     src="https://cdn-icons-png.flaticon.com/512/347/347052.png"
+  //   />
+  // )}
+  // >
+  //   {(childRef: ForwardedRef<HTMLElement>) => (
+  //     <TextInput
+  //       value={value}
+  //       setValue={setValue}
+  //       className="text-class"
+  //       label="text-label:"
+  //       placeholder="text-placeholder"
+  //       autocapitalize="sentences"
+  //       multiline={true}
+  //       ref={childRef}
+  //     />
+  //   )}
+  // </ReplaceInput>
+  // ----------------------------------------------------------
+  // <ReplaceInput>
+  //   {(childRef: ForwardedRef<HTMLElement>) => (
+  //     <NumInput
+  //       value={value}
+  //       setValue={setValue}
+  //       className="num-class"
+  //       label="num-label:"
+  //       max={100}
+  //       invalidClassName="invalid-value"
+  //       ref={childRef}
+  //     />
+  //   )}
+  // </ReplaceInput>
+  // );
 };
 
 export function App() {
@@ -96,8 +97,8 @@ export function App() {
       {/* <DateInputSample /> */}
       {/* <NumInputSample /> */}
       {counter.map((_: number, i: number) => {
-        return <ReplaceTextInputSample key={i} />;
-        // return <NumInputSample key={i} />;
+        // return <ReplaceTextInputSample key={i} />;
+        return <DateInputSample key={i} />;
       })}
     </>
   );
--- src/components-generic/components/text-input.component.tsx
+++ src/components-generic/components/text-input.component.tsx
@@ -9,15 +9,51 @@ import { ForwardedRef, forwardRef, useMemo } from 'preact/compat';
 import * as c from '../services/constants.service';
 import * as u from '../services/utilities.service';
 
+//  Internal constants
+export enum CAPS_OPTIONS {
+  NONE = 'none',
+  CHARS = 'characters',
+  WORDS = 'words',
+  SENTENCES = 'sentences',
+}
+
 /**
  * @description Resizes text input if component is defined as multiline.
- * @param e - InputEvent.
+ * @param e InputEvent.
  */
 const resizeContainer = (e: InputEvent): void => {
   if (!(e.currentTarget instanceof HTMLElement)) return;
   const elem: HTMLElement = e.currentTarget;
   elem.style.height = '1px'; // ensure resize to smaller
   elem.style.height = `${elem.scrollHeight}px`;
+};
+
+/**
+ * @description Capitalizes text based on 3 provided settings:
+ * -  characters: all text is capitalized,
+ * -  words: all words have first letter capitalized,
+ * -  sentences: all full sentences have first letter capitalized.
+ * Does not autocorrect remaining parts of text to allow typing names etc.
+ * @param text What is capitalized.
+ * @param autocapitalize One of capitalization options.
+ * @returns Capitalized text.
+ */
+const capitalizeText = (text: string, autocapitalize: CAPS_OPTIONS) => {
+  switch (autocapitalize) {
+    case CAPS_OPTIONS.CHARS:
+      return text.toUpperCase();
+    case CAPS_OPTIONS.WORDS:
+      return text.replace(
+        /\w+/g,
+        word => word[0].toUpperCase() + word.slice(1)
+      );
+    case CAPS_OPTIONS.SENTENCES:
+      return text.replace(/^\w|\.\s+\w/g, startOfSentence =>
+        startOfSentence.toUpperCase()
+      );
+    default:
+      return text;
+  }
 };
 
 /**
@@ -50,7 +86,7 @@ export const TextInput = forwardRef(function TextInput(
     label = '',
     placeholder = '',
     enabled = true,
-    autocapitalize = 'none',
+    autocapitalize = CAPS_OPTIONS.NONE,
     multiline = false,
   }: {
     value: string;
@@ -60,11 +96,33 @@ export const TextInput = forwardRef(function TextInput(
     label?: string;
     placeholder?: string;
     enabled?: boolean;
-    autocapitalize?: 'none' | 'sentences' | 'words' | 'characters';
+    autocapitalize?: CAPS_OPTIONS;
     multiline?: boolean;
   },
   ref: ForwardedRef<HTMLElement>
 ) {
+  /**
+   * @description Applies autocapitalize to user input.
+   * Is debounced on input to 350ms to allow key-holding and touch typing.
+   * Ensures control retains cursor position.
+   * @param e InputEvent.
+   */
+  let timeoutId: NodeJS.Timeout;
+  const autocapitalizeInput = (e: InputEvent) => {
+    if (timeoutId) clearTimeout(timeoutId);
+    //  Access element
+    if (!(e.currentTarget instanceof HTMLTextAreaElement)) return;
+    const elem: HTMLTextAreaElement = e.currentTarget;
+    //  Capitalize while retaining cursor position
+    timeoutId = setTimeout(() => {
+      const cursorPosition: number = elem.selectionStart;
+      elem.value = capitalizeText(elem.value, autocapitalize);
+      elem.selectionStart = cursorPosition;
+      elem.selectionEnd = cursorPosition;
+      return;
+    }, 350);
+  };
+
   /**
    * @description Stores value into state when switching focus.
    * @param e Text input focus/blur event.
@@ -110,8 +168,10 @@ export const TextInput = forwardRef(function TextInput(
         class={inputClasses}
         placeholder={placeholder}
         disabled={!enabled}
-        autocapitalize={autocapitalize}
-        onInput={multiline ? resizeContainer : undefined}
+        onInput={e => {
+          multiline && resizeContainer(e);
+          autocapitalize !== CAPS_OPTIONS.NONE && autocapitalizeInput(e);
+        }}
         onBlur={storeValue}
         autocomplete="on"
         rows={1} // set default size
